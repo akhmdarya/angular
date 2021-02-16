f9ddc3efe9b0608a67f75415c943aa4be0789997
Darya <akhmdarya@gmail.com>
2021-02-16 16:26:46 +0600

    categories

--- apps/dar/src/app/app-routing.module.ts
+++ apps/dar/src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from "@angular/core";
 import { Route, RouterModule } from "@angular/router";
-// import { ArticlesComponent } from "./pages/articles/articles.component";
+
 import { GreetingComponent } from "./pages/greeting/greeting.component";
-// import { ArticlesComponent } from './articles/articles/articles.component';
+
 
 
 const routes: Route[] = [
--- apps/dar/src/app/categories/categories/categories.component.ts
+++ apps/dar/src/app/categories/categories/categories.component.ts
@@ -12,7 +12,8 @@ import { catchError, map, mergeMap } from 'rxjs/operators';
 })
 export class CategoriesComponent implements OnInit {
 
-  categories$: Observable<Category[]>;
+  categories$: Observable<Category[]>; 
+   articles$: Observable<Article[]>;
   
 
   constructor(
@@ -23,28 +24,10 @@ export class CategoriesComponent implements OnInit {
   ngOnInit(): void {
     this.categories$ = this.httpClient
       .get<Article[]>(`/api/categories`)
- 
+  
+     
 
 
-      // .pipe(
-      //   catchError(() => of(null)),
-      //   mergeMap(articles => (
-      //     !articles ? of(null) :
-      //       this.httpClient.get<Category[]>(`/api/categories`)
-      //         .pipe(
-      //           map(categories => {
-      //             return articles.map(article => {
-      //               const category = categories.find(c => c.id === article.category_id);
-      //               return {
-      //                 ...article,
-      //                 category_title: category ? category.title : 'No category',
-      //               }
-      //             });
-      //           }),
-      //           catchError(() => of(articles))
-      //         )
-      //   ))
-      // )
   }
   rowClickHandlerCat(category: Category) {
     console.log(category)
